cd3717dc1393364a54adf511c0d637e62ea61219
shivkanyagurjar9617 <shivkanyagurjar892@gmail.com>
2022-02-08 08:56:54 +0530

    v1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,67 @@
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { useState } from 'react';
+import { Button, Table } from 'react-bootstrap';
 import './App.css';
 
 function App() {
+  //state
+  const[student,setStudent]=useState({
+    data:[]
+  });
+  //Function
+  let getStudents = (e)=>{ 
+    console.log('good morning');
+  }
+    try {
+      fetch('http://localhost:1337/api/students').then((data)=>{
+        return data.json ();
+      }).then((data)=>{
+        console.log(data);
+        setStudent(data);
+      }).catch((err)=>{
+        console.log(err);
+      });
+
+    } catch (error) {
+      console.log(error);
+    }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+     <Button onClick={(e)=>{ getStudents(e) }}>Get My students</Button>
+   <h1>Read Operation</h1>
+   <Table striped bordered hover>
+          <thead>
+            <tr>
+              <th>#</th>
+             
+              <th>students</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              student.data.map(function(currentValue, index, arr){
+                console.log(arr[index].id);
+                console.log(arr[index].attributes.name);
+                return(
+                  <tr key={index}>
+                  <td>{arr[index].id}</td>
+                  <td>{arr[index].attributes.name}</td>
+                  <td>
+                  <Button variant="success" size="sm">View</Button>{''}
+                  <Button variant="primary" size="sm">Edit</Button>{''}
+                  <Button variant="danger" size="sm">Delete</Button>{''}
+                  </td>
+                  </tr>
+                )
+
+              })
+            }
+          </tbody>
+    
+   </Table>
+   </>
+
   );
 }
 
